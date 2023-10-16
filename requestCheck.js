34042926e47d29717d30df0ebe932c5d5f38ec23
YoonJunHyeon <qmdldos@gmail.com>
2023-10-16 17:29:20 +0900

    결과 값 표시 수정

--- requestCheck.js
+++ requestCheck.js
@@ -3,9 +3,9 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
 
   console.log('URL로 최초 접속하는 트랜잭션 확인하기');
-  console.log(req.url);
+  console.log(req.url); // /
   console.log('URL로 최초 접속하는 요청 유형 확인하기')
-  console.log(req.method);
+  console.log(req.method); // GET
 })
 server.listen(3000, ()=>{
   console.log(`server running : htttp://localhost:3000`)
